7134cc4c1266942f80783f848b20adb4566df8a8
marimum000 <mariana.gubert.ifpr.2023@gmail.com>
2025-08-27 15:17:16 -0300

    multipla escolha
    usamos o alert para o dia da semana correspondente

---
+++ multipla_escolha/js/ex00.js
@@ -0,0 +1,39 @@
+var dia
+
+dia = parseInt(prompt("Digite o número do dia da semana correspondente:"))
+
+switch(dia){
+  case 1:{
+    alert("Domingo")
+    break
+  }
+   case 2:{
+    alert("Segunda-feira")
+    break
+  }
+    case 3:{
+    alert("Terça-feira")
+    break
+  }
+   case 4:{
+    alert("Quarta-feira")
+    break
+  }
+   case 5:{
+    alert("quinta-feira")
+    break
+  }
+   case 6:{
+    alert("Sexta-feira")
+    break
+  }
+   case 7:{
+    alert("Sabado")
+    break
+  }
+  default:{
+    alert("opção inválida")
+    break
+  }
+   
+}
