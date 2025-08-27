50bcf55442123b441d0f4e359c15556d51f04f63
marimum000 <mariana.gubert.ifpr.2023@gmail.com>
2025-08-27 16:17:13 -0300

    atividade2
    se o número digitado pelo usuário for maior que 3 seu triplo é apresentado e se for menor seu dobro.

---
+++ exercicios.html/js/ex02.js
@@ -0,0 +1,11 @@
+var n, t
+n=parseInt(prompt("Digite um número:"))
+
+if(n<=3){
+    t=n*2
+    alert("O dobro de " +n+ " é: " +t)
+}
+  if(n>3){
+    t=n*3
+    alert("O triplo de " +n+ " é: " +t)
+    }
