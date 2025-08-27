d9a009d4af708a15f5bf4cb9bdee7c0fee620fe7
marimum000 <mariana.gubert.ifpr.2023@gmail.com>
2025-08-27 15:54:06 -0300

    lacos repeticao
    nem eu sei

---
+++ Lacos_repeticao/js/ex00.js
@@ -0,0 +1,22 @@
+var cont
+cont=0
+
+while(cont<3){
+    alert("Aborrecendo o usuário " +cont+ "x")
+    cont++
+}
+
+for(cont=0;cont<5; cont++)
+   if(cont==0)
+   {
+     alert("Clique uma vez!" )
+   }
+    else  
+   {
+        alert("Continue clicando!")
+   }
+
+var resposta
+do{
+    resposta = confirme("Deseja continuar?")
+}while(resposta==true)
